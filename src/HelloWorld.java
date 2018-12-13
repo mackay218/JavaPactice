5860b46aad80a98ebe0e999be73024255829ccfc
Dan MacKay <docblues@DanMacKbook.local>
2018-12-13 12:49:26 -0600

    first method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        helloWorldMethod();
+    }
+
+    public static void helloWorldMethod(){
         System.out.println("Hello World");
     }
  }
