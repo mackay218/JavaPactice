446cbbbbc9d7c013a40e0398820ef568d4c0080f
Dan MacKay <docblues@DanMacKbook.local>
2018-12-18 10:48:19 -0600

    added my name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,15 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         helloWorldMethod();
+        myNameIs();
     }
 
     public static void helloWorldMethod(){
         System.out.println("Hello World");
+    }
+
+    public static void myNameIs(){
+        String myName = "Dan";
+        System.out.println("My name is " + myName);
     }
  }
